11386e9d60c460e2a3b3331b262c4752e14d44ab
sherzodr <sherzodr@THINKPAD-T480>
2020-09-29 17:57:32 +0500

    Made changes to the setup.py file; bumbed version number by one

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ from setuptools import setup
 
 setup(
     name="penmon",
-    version="0.1.0",
+    version="0.1.2",
     description="Implementation of Penman-Monteith equation to calculate ET for a reference crop",
+    # long_description_content_type="text/markdown",
+    # long_description=long_description,
     author="Sherzod RUZMETOV",
     author_email="sherzodr@gmail.com",
-    license = "MIT",
+    license="MIT",
+    url="https://github.com/sherzodr/penmon",
+    download_url="https://github.com/sherzodr/penmon/archive/0.1.1.tar.gz",
     py_modules=["penmon.eto"],
     package_dir={'': 'src'},
     packages=["penmon"],
-    keywords=[ "Penman-Monteith", "ET", "evapotranspiration", "plant water demand", "reference crop", "FAO 56" ])
+    keywords=[ "Penman-Monteith", "ET", "evapotranspiration", "plant water demand", "reference crop", "FAO 56" ],
+    python_required='>=3.6',
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Hydrology",
+        "Topic :: Scientific/Engineering :: Atmospheric Science"
+        ]
+    )
 
