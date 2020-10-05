036d0ca72a92db253a40bed6bc4724e960c7c476
sherzodr <sherzodr@activation.cloud.techsmith.com>
2020-10-05 12:15:54 +0500

    Removed redundant information from the source file.

--- src/penmon/eto.py
+++ src/penmon/eto.py
@@ -8,50 +8,8 @@ Penman-Monteith equation is used to calculate reference crop evapotranspiration 
 for a given location using available climate data. This method provides many ways of estimating
 missing climate data using minimal data.
 
-Following are the least data required to estimate ETo:
+@author: sherzodr@gmail.com
 
-    * latitude
-    * elevation
-    * date
-    * daily minimum temperature
-    * daily maximum temperature
-
-It can do this by making certain assumptions about the climate conditions. These assumptions
-can be fine-tuned. **Climate** is responsible for setting these assumptions.
-
-Following is the simplest code that is possible using the module:
-
-    import penmon.eto as pm
-    
-    station = pm.Station(latitude=41.42, altitude=109)
-    day = station.get_day(238)
-    day.temp_min = 19.5
-    day.temp_max = 25.6
-    
-    print("ETo for this day is", day.eto)
-
-By default it assumes we are in *arid, interior location, with moderate winds*. To adjust these assumptions you can define
-your own Climate() class and assign this instance to the station:
-
-    humid_climate = pm.Climate().humid().coastal().strong_winds()
-
-    station = pm.Station(41.42, 109)
-    station.climate = climate
-    
-If you do not want it to make any assumptions about its climate you may set *climate* attribute
-of the Station to *None*:
-
-    station = pm.Station(41.42, 109)
-    station.climate = None
-
-In this case, if humidity and wind information are missing, instead of trying to guess missing data
-it relies on Haargresves' ETo equation (Eq 52). 
-
-Whenever appropriate reference to Equations used in UN-FAO Paper 56 are made.
-
-AUTHOR
-
-    Sherzod Ruzmetov <sherzodr@gmail.com>
 """
 import math
 import datetime as dt
