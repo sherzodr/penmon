8e72c2e489a40e682f0f20436b1328ef75774d0b
Benjamin Schmidt <benschmidt@live.de>
2023-03-20 20:26:45 +0100

    change project structure and more pyproject rules
    - use structure penmon/penmon
    - more elaborate pyproject.toml with many rules

---
+++ penmon/__init__.py
@@ -0,0 +1,2 @@
+from   .eto import (CHECK_RADIATION_RANGE, CHECK_SUNSHINE_HOURS_RANGE, Climate,
+                    Crop, DayEntry, Station)
--- src/penmon/__init__.py
+++
@@ -1,2 +0,0 @@
-from penmon.eto import Station, DayEntry, Climate, Crop
-from penmon.eto import CHECK_RADIATION_RANGE, CHECK_SUNSHINE_HOURS_RANGE
